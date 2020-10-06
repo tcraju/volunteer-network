d4015e7584ee1db57aa60b4cfcb77f0ff0b54b5f
tcraju <tcnajmul@gmail.com>
2020-10-06 11:46:46 +0600

    header link repared

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -22,6 +22,7 @@ const Header = () => {
     const history = useHistory()
     const classes = useStyles();
     const {_id} = useParams();
+    console.log(_id);
     return (
         <Container>
         <AppBar position="static">
@@ -37,8 +38,8 @@ const Header = () => {
                 <Button onClick={() => history.push('/donation')}> Donation </Button>                
                 <Button onClick={() => history.push('/blog')}> Blog </Button>
                 <Button onClick={() => history.push('/taskDetail')}> Appointment </Button>
-                {/* <Button id='reg-btn' onClick={() => history.push(`/registration/${jobId} || '5f7a006f4d5b18438892fb19'`)} > Register </Button> */}
-                <Button id='reg-btn' onClick={() => history.push(_id && `/registration/${_id}`)} > Register </Button>
+                 {_id ? <Button id='reg-btn' onClick={() => history.push(`/registration/'${_id}'`)} > Register </Button>:
+                <Button id='reg-btn' onClick={() => history.push(`/registration/5f7a006f4d5b18438892fb19`)} > Register </Button> }
                 <Button id='admin-btn' onClick={() => history.push('/admin')}>Admin</Button>
             </Toolbar>
         </AppBar>
